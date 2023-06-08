64a8d29c1946f83f6ab12d92e2471b22a62852fc
NilsonAbrantes <lucasnilson624@gmail.com>
2023-06-08 17:14:45 -0300

    start

---
+++ recips/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return HttpResponse("HOME 2")
+
+
+def contato(request):
+    return HttpResponse("Site maneiroooo")
+
+
+def sobre(request):
+    return HttpResponse("Sobreex")
